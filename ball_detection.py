41f61f6717926e9ec9f77c0adaa2192d98c6e186
BiggusMaximus <annastyabagas164@gmail.com>
2023-01-27 09:31:36 +0700

    adding new

--- ball_detection.py
+++ ball_detection.py
@@ -2,10 +2,15 @@ import cv2
 import numpy as np
 import serialCommunication
 
+dist = lambda x1,y1,x2,y2: (x1-x2)**2 + (y1-y2)**2
+prevCircle = None
+
 def detectBall(img):
     height, width = img.shape[:2]
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.GaussianBlur(img_gray, (17, 17), 0)
+    chosen = None
+
     circles = cv2.HoughCircles(
         img_blur, 
         cv2.HOUGH_GRADIENT, 
@@ -19,10 +24,15 @@ def detectBall(img):
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
-            cv2.circle(img, (i[0], i[1]), i[2], (0,255,0), 2)
-            serialCommunication.send_data(f"ball_found,{i[0]}\n")
-    else:
-        serialCommunication.send_data(f"ball_not_found,{0}\n")
+            if chosen is None:
+                chosen = i
+            if prevCircle is not None:
+                if dist(chosen[0], chosen[1], prevCircle[0], prevCircle[1]) <= dist(i[0], i[1], , prevCircle[0], prevCircle[1]):
+                    chosen = i
+        cv2.circle(img, (chosen[0], chosen[1]), i, (0,255,0), 2)
+        #.send_data(f"ball_found,{i[0]}\n")
+        prevCircle = chosen
+    
 
         # print(f"height: {height}, width: {width}, xp: {i[0]}")
     return img
